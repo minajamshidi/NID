2656ba1367a7d28485f028bb28e06216e0b72725
Mina Jamshidi Idaji <45431942+minajamshidi@users.noreply.github.com>
2019-11-06 14:18:32 +0100

    Add files via upload

---
+++ m_main_LEMON_NID.m
@@ -0,0 +1,231 @@
+%% main script for extraction of coupled paris of oscillation from LEMON data
+% This scripts runs on resting-state EEG of young, right-handed men of
+% LEMON dataset. You should download the data beforehand.
+%
+% LEMON paper:
+% Babayan Anahit, Erbey Miray, Kumral Deniz, et al., Scientific Data 6, 180308 (2019).
+%
+% 
+%%
+% This script is a part of the supplementary material for the following paper:
+% 
+% -  Jamshidi Idaji et al,  "Nonlinear Interaction Decomposition (NID): A Method 
+% for Separation of Cross-frequency Coupled Sources in Human Brain". doi: <https://doi.org/10.1101/680397 
+% https://doi.org/10.1101/680397>
+% 
+% 
+% (C) Mina Jamshidi Idaji, Oct 2019, @ MPI CBS, Leipzig, Germany
+% https://github.com/minajamshidi/NID
+% minajamshidi91@gmail.com
+%%
+% *Please cite the above paper (or a future peer-reviewed version) in case of 
+% any significant usage of this script or the simulation pipeline.
+%% 
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+% OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+% AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER
+% OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+% DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+% DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+% IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+% OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+%%
+
+%%
+clc; clear; close all;
+%% set path
+addpath('./NID'); % path of NID function
+
+% add the path of EEGLAB
+
+%the path of preprocessed data
+path_to_data1 = '/data/pt_nro109/Share/EEG_MPILMBB_LEMON/EEG_Preprocessed_BIDS_ID/EEG_Preprocessed/';
+
+% add the path+name of the metafile including subjects information
+metafile_path = '/data/pt_nro109/Share/Behavioural_Data_MPILMBB_LEMON/META_File_IDs_Age_Gender_Education_Drug_Smoke_SKID_LEMON.csv';
+
+path_save = './save_data';
+
+%startup_bbci_toolbox;
+%% parameters related to NID
+
+% frequencies ~~~~~~~~~~~~~~~~~~~~~~~~~~
+F_base = 10;
+FrP = 1;
+FrQ = 2;
+fp = FrP*F_base;
+fq = FrQ*F_base;
+Fs = 250; % sampling frequency
+SynchType = 1;
+
+% number of pairs ~~~~~~~~~~~~~~~~~~~~~~~~~~
+PairsNum1 = 5; % # of synchronized sources
+Ncomp = PairsNum1; % # of components for SSD
+Ncomp_ICA = 2*Ncomp; % # of components for ICA
+
+% SSD options ~~~~~~~~~~~~~~~~~~~~~~~~~~
+opt_SSD = struct;
+opt_SSD.DoSSD = [1 1];
+opt_SSD.Ncomp = Ncomp;
+opt_SSD.fc = [fp fq];
+
+%  ICA options ~~~~~~~~~~~~~~~~~~~~~~~~~~
+opt_ICA = struct;
+opt_ICA.ICA_Method = 'fastICA_E5';%'JADE'; %or 'fastICA'
+opt_ICA.Ncomp_ICA = Ncomp_ICA;
+%% Read data Info
+
+% Here I first find the right handed young subjects
+% -------------------------------------------------------------------------
+T =  readtable(metafile_path); % the IDs are in this csv file
+ID = table2array(T(:,1)); % IDs
+hand = table2array(T(:,4)); % hand info
+age = table2array(T(:,3)); % age info
+gender = table2array(T(:,2)); %gender info
+
+ind_young_male_right = find(strcmp(age,'20-25') | strcmp(age,'25-30') & ...
+     ismember(gender,1) &  strcmp(hand,'right')); % Ind of young_male_righ-handed subjects
+SubjN_young_male_right = numel(ind_young_male_right);%number of young_male_righ-handed subjects
+
+subj_ID = ID(ind_young_male_right); % IDs of right handed young subjects
+
+% -------------------------------------------------------------------------
+%Then look at the available preprocessed subjects' data
+% -------------------------------------------------------------------------
+names = dir([path_to_data1,'*_EC.set']);
+names = {names.name}; % names of all files in preprocessed file
+SubjN_preproc = length(names);%number of available preprocessed subjects
+ID_read = cell(SubjN_preproc,1);%ID of available preprocessed subjects
+for subj = 1:SubjN_preproc
+    ID_read{subj} = names{subj}(1:end-7);
+end
+%% load data and process it
+ID_computed = intersect(ID_read,subj_ID);
+SubjN = length(ID_computed);%# of young_male_righ-handed subjects, for whom we have the preprocessed data
+fprintf('~~~~~~~~~~~~~~~~~~~~~~~~~\n~~~~~~~~~~~~~~~~~~~~~~~~~\n');
+fprintf('There are totally %d young male right-handed subjects\n',SubjN);
+fprintf('---------------------------\n---------------------------\n---------------------------\n');
+PermNum = 1000; %# of permutation iterations for sattistical testing of synchronization
+
+% -------------------------------------------------------------------------
+% Build information containers:D:P
+% -------------------------------------------------------------------------
+load('LEMON_chanlocs61.mat'); %this chanlocs file is saved from a subject with 61 channels
+CC_All = NaN(PairsNum1,SubjN);
+PLV_sig = cell(SubjN,1);
+
+% filter coefficients -------------------------------------------------
+[butter_base_b,butter_base_a] = butter(2,[F_base-1 F_base+1]/(Fs/2));
+[butter_p_b,butter_p_a] = butter(2,[FrP*F_base-1 FrP*F_base+1]/(Fs/2));
+[butter_q_b,butter_q_a] = butter(2,[FrQ*F_base-1 FrQ*F_base+1]/(Fs/2));
+    
+for subj = 1:SubjN
+    ID_subj = ID_computed{subj};
+    fprintf('~~~~~~~~~~~~~~~~~~~~~~~~~\n~~~~~~~~~~~~~~~~~~~~~~~~~\n');
+    fprintf('Subject number %d/%d with ID %s\n',subj,SubjN,ID_subj);
+    fprintf('~~~~~~~~~~~~~~~~~~~~~~~~~\n');
+    % load the data of current subject ------------------------
+    SubjName = [ID_computed{subj},'_EC.set'];
+    file_name = sprintf([path_to_data1,'/%s'], SubjName);
+    eeg = pop_loadset('filename', file_name);
+    
+    % if any channel is missing: interpolate the channel --------------
+    
+    [~,bad_elec] = setdiff({chanlocs61.labels},{eeg.chanlocs.labels}); % check the existance of a bad channel
+    if numel(bad_elec) % if there is a bad channel
+        EEG = pop_interp(eeg, chanlocs61, 'spherical'); % interpolate it
+    else
+        EEG = eeg;
+    end
+    
+    % set variables for permutation ---------------------------------
+    
+    % in the permutation part, we segment the signal and permute the
+    % segments. Therefore there should be integer number of segments. Here
+    % we remove the part of X that cannot be in a complete segment.
+    
+    X = double(EEG.data)';
+    L = size(X,1); % the length of the signal
+    Ls = 1*Fs; % 1s segment length
+    Ns = floor(L/Ls); % # of segments
+    X = X(1:Ns*Ls,:); % remove the parts that cannnot be segmented
+    
+    
+    %% METHOD   
+   
+    % X -> Time x Channel
+    [out_ssd] = SSD_BP(X,Fs,opt_SSD,1,[]);
+    
+    %perm------------------
+    %the first iteration is non-permuted
+    CC_pairsNum = NaN(PairsNum1,PermNum+1);
+    
+    ind_perm = 0;
+    while ind_perm <= PermNum
+        fprintf('***Subject %d/%d, perm %d try*****\n',subj,SubjN,ind_perm+1);
+        X_p_ssd = out_ssd.X_NB{1};
+        X_q_ssd = out_ssd.X_NB{2};
+        if ind_perm>0 % the first iteration is the unpermuted data results
+            p2 = randperm(Ns);
+            for k = 1:PairsNum1
+                x2 = reshape(X_p_ssd(:,k),Ls,Ns);
+                y2 = reshape(X_q_ssd(:,k),Ls,Ns);
+                y2 = y2(:,p2);
+                X_q_ssd(:,k) = y2(:);
+            end
+        end
+        out_ssd.X_Aug = [X_p_ssd'; X_q_ssd'];
+        out_ssd.X_NB{2} = X_q_ssd;
+        
+        try
+                        
+           [Success,A_final_p,A_final_q,out_ICA,ICA_Method,D_synch,SynchFac_mean,out_ssd,IDX_src,Xp,Xq] = ...
+                NID(X,FrP,FrQ,F_base,'source_set_num',PairsNum1,'ssd',out_ssd,'fs',Fs,'synchtype',1);
+            if Success
+                ind_perm = ind_perm + 1;
+                fprintf('***Subject %d/%d, perm %d succeeded*****\n',subj,SubjN,ind_perm);
+                CC_pairsNum(:,ind_perm) = D_synch';
+                A_final1 = cell(1,2);
+                A_final1{1} = A_final_p;
+                A_final1{2} = A_final_q;
+                %~~~~~~~~~~~~~~~~
+                if ind_perm==1
+                    out_ssd1 = out_ssd;
+                    out_ICA1 = out_ICA;
+                    SynchFac_pq1 = D_synch;
+                    Xp1 = Xp;
+                    Xq1 = Xq;
+                    IDX_src1 = IDX_src;
+                    ICA_Method1 = ICA_Method;
+                    CC_All(:,subj) = D_synch';
+                end
+                
+            end
+            
+            
+        catch
+            fprintf('ICA failed in the first iter\n');
+        end
+    end % end ind_perm
+   
+    % finding the significant pairs ---------------------------------------
+    PLV1 = CC_pairsNum(:,1);
+    PLV_perm = max(CC_pairsNum(:,2:end));
+    Perc = NaN(5,1);
+    for p = 1:5
+        Perc(p) = sum(PLV_perm>=PLV1(p))/(PermNum/100);
+    end
+    ind1 = find(Perc<5); % significance level 0.05
+    PLV_sig{subj} = PLV1(ind1);
+    
+    % Saving --------------------------------------------------------------
+    %Name_save = sprintf('%s/%s_NID_fp_%d_fq_%d_perm.mat',path_save,ID_subj,FrP*F_base,FrQ*F_base);
+    %save(Name_save, 'out_ssd1','out_ICA1','A_final1',...
+    %    'SynchFac_pq1','Xp1','Xq1','CC_pairsNum','ICA_Method1');
+   
+    
+    
+    %%
+end
+%%
+
---
+++ main_simulations_NID.m
@@ -0,0 +1,255 @@
+%% main script for running NID on synthetic data
+% this script generates simulated EEG with METH toolbox, and then applies
+% NID on it, in order to extract the coupling components. Then calculate 
+% the errors in Monte Carlo runs.
+
+% Note that in this script the results are computed but there is no graph
+% in the output.
+% --------------------------
+% there are four loops:
+% 1.loop on SynchType
+% 2. loop on frequencies of fast and slow oscillations,
+% 3. loop on different SNRs,
+% 4.loop for doing the analysis in multiple iteration, in order to have box
+% plots.
+
+%To control the number of iterations, look at variables FrP1 and FrQ1 for
+%the first loop, and SNRdb for the second loop. iterN variable determines
+% the number of iterations of third loop.
+% --------------------------
+% by changing the variable SynchType, the type of coupling can be
+% controlled.
+
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%% other toolboxes:
+% * fastICA function of fastICA toolbox is modified to include other contrast fuctions.
+
+% * METH toolbox by Guido Nolte is used in this script
+% https://www.uke.de/english/departments-institutes/institutes/neurophysiology-and-pathophysiology/research/research-groups/index.html 
+% (as of 06/11/2019)
+
+% JADE
+
+% 
+%%
+% This script is a part of the supplementary material for the following paper:
+% 
+% -  Jamshidi Idaji et al,  "Nonlinear Interaction Decomposition (NID): A Method 
+% for Separation of Cross-frequency Coupled Sources in Human Brain". doi: <https://doi.org/10.1101/680397 
+% https://doi.org/10.1101/680397>
+% 
+% 
+% (C) Mina Jamshidi Idaji, Oct 2019, @ MPI CBS, Leipzig, Germany
+% https://github.com/minajamshidi/NID
+% minajamshidi91@gmail.com
+% +++++++++++++++
+% This code is based on the code used in:
+
+% Nikulin, V.V., Nolte, G., Curio, G., 2012. Cross-frequency decomposi-
+% tion: A novel technique for studying interactions between neuronal oscilla-
+% tions with different frequencies. Clinical Neurophysiology 123, 1353–1360.
+% doi:10.1016/j.clinph.2011.12.004.
+%%
+% *Please cite the above papers (or a future peer-reviewed version) in case of 
+% any significant usage of this script or the simulation pipeline.
+%% 
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+% OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+% AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER
+% OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+% DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+% DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+% IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+% OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+%%
+clc; clear; close all;
+%% set path
+
+% add the path of METH toolbox here:
+METH_path = '/data/pt_02076/MATLAB_Toolboxes/meth/';
+
+addpath('./simulate_EEG')
+addpath(genpath('./NID'));
+addpath(genpath(METH_path))
+%% parameters
+% frequencies ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+F_base = 10;
+FrP1 = 1;
+FrQ1 = 2;
+pq_N = length(FrP1);
+Fs = 500; % sampling frequency
+T_sec = 5*60; % length of each signal segment in seconds.
+
+% Coupling Properties ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+SynchType = 1; % parameter setting the type of coupling
+kappa1 = 10;
+kappa = kappa1*0.1; % variable controling the strength of coupling
+
+%       - SynchType: the type of synchronization 
+%               0: same amplitude, 100% locked phase
+%               1: independent amplitude, 100% locked phase
+%               2: independent amplitude, locked phase (strength of synch controled with kappa von mesis)
+                % for this option you will need CircStat toolbox by Philipp Berens & Marc J. Velasco
+                % https://de.mathworks.com/matlabcentral/fileexchange/10676-circular-statistics-toolbox-directional-statistics
+                % (as of 06/11/2019)
+%                 kappa is the kappa parameter in von mises distribution.
+%                 larger kappa -> more peaky von mises -> less jitter -> stronger coupling
+%               3: Correlated amplitudes, independent phases (strength of synch controled with kappa)
+%                  A2 = kappa*A1 + (1-kappa)*(white Noise)
+%                  Thus, kappa larger (near 1) means that the coupling strength is higher
+%               4: phase-amplitude coupling
+%               5: 2kinds, 1st phase locked, 2nd amp locked
+%               6: independent amplitude, independent phases
+%               7: nothing. only noise
+%       - kappa: the parameter related to the strength of synchronization. 
+%       (only used for SynchType=2,3, otherwise put it as 0). 
+
+DiffTopo = 1; % indicates if the topographies of the sources should be generated randomly (1) or should be the same (0).
+PhaseLag_q = 0; %phase lag of source at fast frequency
+
+% number of pairs ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+PairsNum = 2; % number of of synchronized sources
+NoiseNum = 125; % number of noise sources
+% SNR ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+SNRdb = -5; % in db
+SNR1 = 10.^(SNRdb/10);
+SNR_N = length(SNRdb);
+
+% Save and Plot ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+DoSave = 0; % save?
+save_path = './'; %where?
+
+% Iteration settings ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+iterN = 100; % number of iterations
+
+tic
+%%
+for ind_pq = 1: pq_N % loop on fp and fq
+    
+    % Settings for each (fp,fq) couple ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    FrP = FrP1(ind_pq);
+    FrQ = FrQ1(ind_pq);
+    fp = FrP*F_base; % slow frequency
+    fq = FrQ*F_base; % fast frequency
+    rng('shuffle')
+    seed1 = randperm(9999999,4*iterN);
+    seed1 = reshape(seed1,4,iterN);
+    
+    
+    for ind_snr = 1:SNR_N % loop on SNR
+        
+        % Settings for each SNR value ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        if SNRdb(ind_snr)<0 % this is for the purpose of save name
+            MinPos = 'minus';
+        else
+            MinPos = 'pos';
+        end
+        ep_NID = NaN(iterN,PairsNum); % errors of slow oscillations
+        eq_NID = NaN(iterN,PairsNum); % errors of fast oscillations
+        ep_SSD = NaN(iterN,PairsNum);
+        eq_SSD = NaN(iterN,PairsNum);
+        D_synch = NaN(iterN,PairsNum);
+        SynchFac_mean = NaN(iterN,1);
+        ICA_Method = cell(iterN,1);
+        dipoles = cell(iterN,1);
+        n_catch = 0;
+        for ind_iter = 1:iterN
+            try
+                fprintf('***** SNR=%d (db) **** iter num.=%d\n',SNRdb(ind_snr),ind_iter)
+                %% make synth. signal
+                disp('Making the signals....');
+                rng(seed1(ind_iter),'twister')% we make the same signal for different SNRs, just modify the signal powers with regards to SNR
+                
+                [X, src_p, src_q, pattern_p, pattern_q, electrode_locs, dipoles{ind_iter},~, ~] = ...
+                    Produce_EEG_CFC(PairsNum, NoiseNum, F_base, FrP, FrQ, Fs,SNR1(ind_snr),DiffTopo,...
+                    SynchType,kappa,PhaseLag_q,T_sec, [], []);
+                % ------------------------------
+                % X is Time*channel
+                % X is bandpass-filtered in [0.5,50]
+                % ------------------------------
+                
+                % ----------------------------------
+                % to plot the PSD of the signal, you can use pwelch or
+                % plot_sepec from https://github.com/minajamshidi/EEG-Functions-MATLAB
+                % figure
+                % plot_spec(X, Fs, 'frequency_mark', [10,20], 'freq_res', 0.5, 'noverlap', Fs, 'f_max',50)
+                % set(gca, 'fontweight','bold', 'fontsize', 14)
+                
+                % ----------------------------------
+                % to plot the mixing patterns, use this function of METH:
+                % figure, showfield(pattern_p(:,1), electrode_locs);
+                
+                % ----------------------------------
+                % Test the synchronization coefficients: 
+                % PLValue = Phase_Locking(src_p,src_q,FrP,FrQ)
+                % CorrAbs = corr(abs(hilbert(src_p)),abs(hilbert(src_q)))
+                % Corr_PAC = corr(abs(angle(hilbert(src_p))),abs(hilbert(src_q)))
+
+                
+                %% NID
+                [Success,A_final_p,A_final_q,out_ICA,ICA_Method{ind_iter},D_synch(ind_iter,:),SynchFac_mean(ind_iter),out_ssd] = ...
+                    NID(X,FrP,FrQ,F_base,'source_set_num',PairsNum,'ssd_or_bp',[1 1],'fs',Fs,'synchtype',SynchType);
+                
+                
+                Error_p = Pattern_Angle( pattern_p, A_final_p);
+                Error_q = Pattern_Angle(pattern_q, A_final_q);
+                % --------
+                
+                Perm1 = perms(1:PairsNum); % all permutations
+                
+                P1 = Error_p; % errors of selected components at fp
+                Q1 = Error_q; % errors of selected components at fq
+                PQ1 = P1.^2 + Q1.^2;
+                Er = NaN(size(Perm1,1),1);
+                for k = 1:size(Perm1,1) %for all permutations
+                    E = 0;
+                    for j = 1:PairsNum
+                        E = E + PQ1(Perm1(k,j),j); %sum of square errors
+                    end
+                    Er(k) = E/PairsNum; %MSE
+                end
+                [~,IDX] = min(Er); % permutation with min MSE
+                IDX_patt2 = Perm1(IDX,:);
+                
+                % save the errors of selected components ~~~~~~~~~~~~~~~~~~~~~~~~~~
+                % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                idx_error = sub2ind(size(Error_p), IDX_patt2(1:PairsNum), 1:PairsNum);
+                ep_NID(ind_iter,:) = Error_p(idx_error);
+                eq_NID(ind_iter,:) = Error_q(idx_error);
+                
+                             
+                
+                %% plot the topo
+                disp('~~~~~~~~');
+                for j = 1:PairsNum
+                    disp(['Source P, Pair ',num2str(j),', error=',num2str(Error_p(IDX_patt2(j),j))])
+                    disp(['Source Q, Pair ',num2str(j),', error=',num2str(Error_q(IDX_patt2(j),j))])
+                    
+                end
+                %%
+                disp('~~~~~~~~');
+            catch
+                n_catch = n_catch + 1;
+            end
+        end %for ind_iter
+        
+        %% save
+        if DoSave
+            Name = sprintf('Data_iterN_%d_fp_%d_fq_%d_SynchType_%d_%dpairs_Fs_%d_SNR_%s%d_Tsec_%d',...
+                iterN,fp,fq,SynchType,PairsNum,Fs,MinPos,abs(SNRdb(ind_snr)),T_sec);
+            Name_save = [save_path,Name,'_Result','.mat'];
+            save(Name_save,'ep_NID','eq_NID','seed1',...
+                'SynchFac_mean','D_synch','ICA_Method', 'dipoles');
+        end
+    end % end ind_snr
+end % ind_pq
+toc
