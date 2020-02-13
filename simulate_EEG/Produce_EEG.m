24c93b4b8ff61ef6c054051befaca04bf7dd08a2
Mina Jamshidi Idaji <45431942+minajamshidi@users.noreply.github.com>
2020-02-13 15:37:18 +0100

    Update Produce_EEG.m

--- simulate_EEG/Produce_EEG.m
+++ simulate_EEG/Produce_EEG.m
@@ -27,8 +27,7 @@ function [ eeg, pattern_sig, electrode_locs ] = Produce_EEG(SourceNum, Fs,SNR, s
 % This script is a part of the supplementary material for the following paper:
 % 
 % -  Jamshidi Idaji et al,  "Nonlinear Interaction Decomposition (NID): A Method 
-% for Separation of Cross-frequency Coupled Sources in Human Brain". doi: <https://doi.org/10.1101/680397 
-% https://doi.org/10.1101/680397>
+% for Separation of Cross-frequency Coupled Sources in Human Brain". doi: <https://doi.org/10.1016/j.neuroimage.2020.116599>
 % 
 % 
 % (C) Mina Jamshidi Idaji, Oct 2019, @ MPI CBS, Leipzig, Germany
@@ -42,7 +41,7 @@ function [ eeg, pattern_sig, electrode_locs ] = Produce_EEG(SourceNum, Fs,SNR, s
 % tions with different frequencies. Clinical Neurophysiology 123, 1353–1360.
 % doi:10.1016/j.clinph.2011.12.004.
 %%
-% *Please cite the above papers (or a future peer-reviewed version) in case of 
+% *Please cite the above papers in case of 
 % any significant usage of this script or the simulation pipeline.
 %% 
 % THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
